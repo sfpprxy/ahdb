427864b3286e802028da684cf78e5107b2e129b1
Joe <rxyjoey@gmail.com>
2019-09-23 10:44:17 +0800

    add ui

--- src/main/AHDBApp.go
+++ src/main/AHDBApp.go
@@ -1,38 +1,39 @@
 package main
 
 import (
+	"github.com/andlabs/ui"
 	_ "github.com/andlabs/ui/winmanifest"
 	"github.com/getlantern/systray"
 	"github.com/getlantern/systray/example/icon"
-	_ "log"
+	"log"
 )
 
 func main() {
 	systray.Run(onReady, onExit)
 
-	//err := ui.Main(func() {
-	//	window := ui.NewWindow("Auction House Database App", 500, 500, true)
-	//	box := ui.NewVerticalBox()
-	//
-	//	chooseButton := ui.NewButton("选择文件")
-	//	box.Append(chooseButton, true)
-	//
-	//
-	//	window.SetChild(box)
-	//	window.SetMargined(true)
-	//	window.OnClosing(func(*ui.Window) bool {
-	//		ui.Quit()
-	//		return true
-	//	})
-	//	ui.OnShouldQuit(func() bool {
-	//		window.Destroy()
-	//		return true
-	//	})
-	//	window.Show()
-	//})
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
+	err := ui.Main(func() {
+		window := ui.NewWindow("Auction House Database App", 500, 500, true)
+		box := ui.NewVerticalBox()
+
+		chooseButton := ui.NewButton("选择文件")
+		box.Append(chooseButton, true)
+
+
+		window.SetChild(box)
+		window.SetMargined(true)
+		window.OnClosing(func(*ui.Window) bool {
+			ui.Quit()
+			return true
+		})
+		ui.OnShouldQuit(func() bool {
+			window.Destroy()
+			return true
+		})
+		window.Show()
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func onReady() {
