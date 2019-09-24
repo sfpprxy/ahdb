04757fc97b96603583c5b5465a7170b074adb286
Joe <rxyjoey@gmail.com>
2019-09-24 22:00:07 +0800

    add win path

--- src/main/client/dataselector_test.go
+++ src/main/client/dataselector_test.go
@@ -6,7 +6,10 @@ import (
 
 func TestSelectValuableDataByAccount(t *testing.T) {
 	changedTsmfilesByAccount := make(map[string]string)
-	changedTsmfilesByAccount["178635643#2"] = "/Applications/World of Warcraft/" +
-		"_classic_/WTF/Account/178635643#2/SavedVariables/TradeSkillMaster.lua"
+	winPath := "D:\\Game\\World of Warcraft\\_classic_\\WTF\\Account\\178635643#2\\SavedVariables\\TradeSkillMaster.lua"
+	macPath := "/Applications/World of Warcraft/_classic_/WTF/Account/178635643#2/SavedVariables/TradeSkillMaster.lua"
+	log.Debug("path: ", winPath, macPath)
+
+	changedTsmfilesByAccount["178635643#2"] = winPath
 	selectValuableDataByAccount(changedTsmfilesByAccount)
 }
--- src/main/client/watcher_test.go
+++ src/main/client/watcher_test.go
@@ -6,8 +6,11 @@ import (
 )
 
 func TestFilterChangedTsmFiles(t *testing.T) {
-	found := findTsmFiles("/Applications/World of Warcraft/_classic_/WoW.mfil")
-	filterChangedTsmFilesByAccount(found)
+	winPath := "D:\\Game\\World of Warcraft\\_classic_\\Wow.exe"
+	macPath := "/Applications/World of Warcraft/_classic_/WoW.mfil"
+	log.Debug("path: ", winPath, macPath)
 
+	found := findTsmFiles(winPath)
+	filterChangedTsmFilesByAccount(found)
 	//t.Error() // to indicate test failed
 }
