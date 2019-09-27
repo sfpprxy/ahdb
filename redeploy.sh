467aa50536aabddd5370f3492f204aee392f7d90
Joe <rxyjoey@gmail.com>
2019-09-27 15:42:36 +0800

    change redeploy

--- redeploy.sh
+++ redeploy.sh
@@ -9,5 +9,4 @@ kill $(cat ./AHDBPID)
 
 echo '> start'
 
-cd ./apps-living-server/
-nohup mvn clean spring-boot:run -Dprofile=test -Dserver.port=9999./server.log &
+nohup mvn clean spring-boot:run -Dprofile=test -Dserver.port=9999./ahdbserver.log &
