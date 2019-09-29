2dd8eee5db25a11e84940f273635dd6db8935f69
Joe <rxyjoey@gmail.com>
2019-09-29 10:25:31 +0800

    fix build

--- src/main/client/gobuild.sh
+++ src/main/client/gobuild.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-go build -ldflags="-H windowsgui" -o AHDBApp.exe $(find . -name "*.go" -and -not -name "*_test.go" -maxdepth 1)
+go build -ldflags="-H windowsgui" -o AHDBApp.exe $(find . -name "*.go" -and -not -name "*_test.go")
 
-rcedit-x64.exe "AHDBApp.exe" --set-version-string FileDescription "拍卖数据库"
- rcedit-x64.exe "AHDBApp.exe" --set-version-string ProductName "拍卖数据库"
+./rcedit-x64.exe "AHDBApp.exe" --set-version-string FileDescription "拍卖数据库"
+./rcedit-x64.exe "AHDBApp.exe" --set-version-string ProductName "拍卖数据库"
 
- rcedit-x64.exe "AHDBApp.exe" --set-product-version "0.0.2.1"
-rcedit-x64.exe "AHDBApp.exe" --set-file-version "0.0.2.1"
+./rcedit-x64.exe "AHDBApp.exe" --set-product-version "0.0.2.1"
+./rcedit-x64.exe "AHDBApp.exe" --set-file-version "0.0.2.1"
 
- rcedit-x64.exe "AHDBApp.exe" --set-icon "ahdb.ico" 
+./rcedit-x64.exe "AHDBApp.exe" --set-icon "ahdb.ico"
