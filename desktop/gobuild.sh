7d58ff672b4dafb88c4872f1f8b1432fed3680ef
Joe <rxyjoey@gmail.com>
2019-10-21 17:23:59 +0800

    rename desktop product as 地精传送器

--- desktop/gobuild.sh
+++ desktop/gobuild.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-go build -ldflags="-H windowsgui" -o AHDBApp.exe $(find . -name "*.go" -and -not -name "*_test.go")
+go build -ldflags="-H windowsgui" -o 地精传送器.exe $(find . -name "*.go" -and -not -name "*_test.go")
 
-./rcedit-x64.exe "AHDBApp.exe" --set-version-string FileDescription "地精传送器"
-./rcedit-x64.exe "AHDBApp.exe" --set-version-string ProductName "地精传送器"
+./rcedit-x64.exe "地精传送器.exe" --set-version-string FileDescription "地精传送器"
+./rcedit-x64.exe "地精传送器.exe" --set-version-string ProductName "地精传送器"
 
-./rcedit-x64.exe "AHDBApp.exe" --set-product-version "0.0.3.0"
-./rcedit-x64.exe "AHDBApp.exe" --set-file-version "0.0.3.0"
+./rcedit-x64.exe "地精传送器.exe" --set-product-version "0.0.3.0"
+./rcedit-x64.exe "地精传送器.exe" --set-file-version "0.0.3.0"
 
-./rcedit-x64.exe "AHDBApp.exe" --set-icon "ahdb.ico"
+./rcedit-x64.exe "地精传送器.exe" --set-icon "ahdb.ico"
--- desktop/gorun.sh
+++ desktop/gorun.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-go run AHDBApp.go $(find . -name "*.go" -and -not -name "*_test.go" -and -not -name "AHDBApp.go" -maxdepth 1) "$@"
+go run ahdbapp.go $(find . -name "*.go" -and -not -name "*_test.go" -and -not -name "ahdbapp.go" -maxdepth 1) "$@"
