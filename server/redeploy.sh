41904e6b7f195001812d09fc435d7491902014e7
Joe <rxyjoey@gmail.com>
2020-08-31 15:19:11 +0800

    update travis

--- server/redeploy.sh
+++ server/redeploy.sh
@@ -1,19 +1,8 @@
 #!/usr/bin/env bash
-echo '> stash'
-git stash
-
-echo '> pull'
-git pull
-
-cp ./src/main/resources/logback.xml.prod ./src/main/resources/logback.xml
-cp ./src/main/resources/application.properties.prod ./src/main/resources/application.properties
-
-echo '> kill old'
-kill $(cat ./AHDBPID)
+echo '> env keys'
+echo myname is ${myname}
+echo ${myname}
 
 echo '> package'
-mvn -Dmaven.test.skip=true package
 
 echo '> start'
-nohup java -Xmx128m -Dprofile=prod -Dserver.port=9999 \
--jar target/ahdbserver-1.3.1.jar  >> ./ahdbserver.log &
