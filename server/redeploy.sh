f005de94a3ac4b7aaf7e64b8e69eed3799b9931b
Joe <rxyjoey@gmail.com>
2020-09-01 17:21:48 +0800

    update .travis.yml 2020-09-01 17:21:46

--- server/redeploy.sh
+++ server/redeploy.sh
@@ -7,5 +7,5 @@ pwd
 #./mvnw package -Pnative
 
 echo '> deploy'
-ls ./target/ahdbserver-1.3.2-SNAPSHOT-runner
+ls ./target
 scp -P4422 ./target/ahdbserver-1.3.2-SNAPSHOT-runner root@"$SERVER_HOST":~/ahdb/server
