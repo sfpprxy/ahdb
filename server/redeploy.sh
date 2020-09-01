f3309dbb3a3d469976486e83a11572ef543844cf
Joe <rxyjoey@gmail.com>
2020-09-01 17:04:04 +0800

    update .travis.yml try build scp

--- server/redeploy.sh
+++ server/redeploy.sh
@@ -1,35 +1,11 @@
 #!/usr/bin/env bash
+echo "> pwd"
 pwd
-
-  # Install GraalVM with SDKMAN
-  curl -s "https://get.sdkman.io" | bash
-  source "$HOME/.sdkman/bin/sdkman-init.sh"
-  sdk install java 20.2.0.r11-grl
-
-  # Check if GraalVM was installed successfully
-  java -version
-
-#  # Install Maven, that uses GraalVM for later builds
-#  sdk install maven
-#
-#  # Show Maven using GraalVM JDK
-#  mvn --version
-#
-#  # Install GraalVM Native Image
-#  gu install native-image
-#
-#  # Check if Native Image was installed properly
-#  native-image --version
-
-echo '> env keys'
-
-echo "$SERVER_HOST"
 
 echo '> package'
 ./mvnw -version
-#mvn package -Pnative
+./mvnw package -Pnative
 
-echo '> start'
-touch rq
-cat "mama" > rq
-scp -P4422 rq root@"$SERVER_HOST":~/ahdb/server
+echo '> deploy'
+ls ./target/ahdbserver-1.3.2-SNAPSHOT-runner
+scp -P4422 ./target/ahdbserver-1.3.2-SNAPSHOT-runner root@"$SERVER_HOST":~/ahdb/server
