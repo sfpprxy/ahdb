2214206a32cdb73640a0d8114b8c8ed87fd2f3e4
Joe <rxyjoey@gmail.com>
2020-09-01 16:52:12 +0800

    update .travis.yml 10

--- server/redeploy.sh
+++ server/redeploy.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
 pwd
 
+  # Install GraalVM with SDKMAN
+  curl -s "https://get.sdkman.io" | bash
+  source "$HOME/.sdkman/bin/sdkman-init.sh"
+  sdk install java 20.2.0.r11-grl
+
+  # Check if GraalVM was installed successfully
+  java -version
+
+#  # Install Maven, that uses GraalVM for later builds
+#  sdk install maven
+#
+#  # Show Maven using GraalVM JDK
+#  mvn --version
+#
+#  # Install GraalVM Native Image
+#  gu install native-image
+#
+#  # Check if Native Image was installed properly
+#  native-image --version
+
 echo '> env keys'
 
 echo "$SERVER_HOST"
