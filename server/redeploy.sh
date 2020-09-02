be2b56c3c714701944f81de1fac4f3f65018925a
Joe <rxyjoey@gmail.com>
2020-09-02 09:53:38 +0800

    update .travis.yml

--- server/redeploy.sh
+++ server/redeploy.sh
@@ -14,13 +14,19 @@ wget https://hub.fastgit.org/Mikubill/transfer/releases/download/v0.4.7/transfer
 tar xzvf transfer_0.4.7_linux_amd64.tar.gz
 
 echo '> upload jar'
-./transfer cow ./target/ahdbserver-1.3.2-SNAPSHOT-runner
-# todo get link
+#./transfer cow ./target/ahdbserver-1.3.2-SNAPSHOT-runner | grep 'Download Link: ' | cut -d' ' -f3 >> link.txt
+echo "Some text here." > myfile.txt
+./transfer cow myfile.txt | grep 'Download Link: ' | cut -d' ' -f3 >> link.txt
+link=$(cat link.txt)
+echo link on travis
+echo "$link"
 
 echo '> login remote'
-ssh -p 4422 ./travis_rsa root@$SERVER_HOST pwd
+ssh -p 4422 ./travis_rsa root@"$SERVER_HOST" pwd
+echo link on remote
+echo "$link"
 
 echo '> copy runner'
-scp -P4422 ./target/ahdbserver-1.3.2-SNAPSHOT-runner root@"$SERVER_HOST":~/ahdb/server
+#scp -P4422 ./target/ahdbserver-1.3.2-SNAPSHOT-runner root@"$SERVER_HOST":~/ahdb/server
 
 echo '> copy runner ok'
