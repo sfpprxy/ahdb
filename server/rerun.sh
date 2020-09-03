aa2dde7ee11ede332fa7f93cd90a409c8e9e12c9
Joe <rxyjoey@gmail.com>
2020-09-03 10:25:15 +0800

    fix rerun.sh

--- server/rerun.sh
+++ server/rerun.sh
@@ -12,6 +12,3 @@ kill $(cat ./pid)
 
 echo '> start'
 nohup ./ahdbserver-1.3.2-SNAPSHOT-runner -XX:+PrintGC -XX:+PrintGCTimeStamps -XX:+VerboseGC +XX:+PrintHeapShape -Xmx32m >> ./ahdbserver.log & echo $! > pid
-
-
-./mvnw package -Pnative -Dquarkus.native.additional-build-args=-H:IncludeResources=META-INF/resources/.* -Dquarkus.native.enable-http-url-handler=true -Dquarkus.native.auto-service-loader-registration=true
