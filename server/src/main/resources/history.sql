2265229e28409ef45b5f2b5afe6fbae3ab4f4b80
Joe <rxyjoey@gmail.com>
2020-11-05 14:44:41 +0800

    add DDL of views

--- server/src/main/resources/history.sql
+++ server/src/main/resources/history.sql
@@ -1,3 +1,6 @@
+-- PostgreSQL 11.9
+-- timescaledb 1.4.2
+
 -- date 2019-10-04 23:43:31
 create table raw_log
 (
@@ -96,3 +99,38 @@ CREATE TABLE biz_cache
     id        text,
     cache_str text
 );
+
+-- date 2020-11-05 14:33:51
+CREATE VIEW view_hourly(item_id, hourly_time_bucket, hourly_avg_min_buyout, hourly_avg_market_value,
+                        hourly_avg_quantity, hourly_avg_num_auctions) AS
+SELECT item_scan.item_id,
+       time_bucket('01:00:00'::interval, item_scan.scan_time) AS hourly_time_bucket,
+       avg(item_scan.min_buyout)                              AS hourly_avg_min_buyout,
+       avg(item_scan.market_value)                            AS hourly_avg_market_value,
+       avg(item_scan.quantity)                                AS hourly_avg_quantity,
+       avg(item_scan.num_auctions)                            AS hourly_avg_num_auctions
+FROM item_scan
+GROUP BY item_scan.item_id, (time_bucket('01:00:00'::interval, item_scan.scan_time));
+
+CREATE MATERIALIZED VIEW view_daily AS
+WITH bounds AS (
+    SELECT view_hourly_1.item_id                                                                        AS id0,
+           time_bucket('1 day'::interval, view_hourly_1.hourly_time_bucket)                             AS daily_time_bucket0,
+           (avg(view_hourly_1.hourly_avg_market_value) - stddev(view_hourly_1.hourly_avg_market_value)) AS low,
+           (avg(view_hourly_1.hourly_avg_market_value) + stddev(view_hourly_1.hourly_avg_market_value)) AS high
+    FROM view_hourly view_hourly_1
+    GROUP BY view_hourly_1.item_id, (time_bucket('1 day'::interval, view_hourly_1.hourly_time_bucket))
+)
+SELECT view_hourly.item_id,
+       time_bucket('1 day'::interval, view_hourly.hourly_time_bucket) AS daily_time_bucket,
+       avg(view_hourly.hourly_avg_min_buyout)                         AS daily_avg_min_buyout,
+       avg(view_hourly.hourly_avg_market_value)                       AS daily_avg_market_value,
+       avg(view_hourly.hourly_avg_quantity)                           AS daily_avg_quantity,
+       avg(view_hourly.hourly_avg_num_auctions)                       AS daily_avg_num_auctions
+FROM view_hourly,
+     bounds
+WHERE ((view_hourly.hourly_time_bucket < (now())::date) AND (view_hourly.item_id = bounds.id0) AND
+       (time_bucket('1 day'::interval, view_hourly.hourly_time_bucket) = bounds.daily_time_bucket0) AND
+       ((view_hourly.hourly_avg_market_value >= bounds.low) AND
+        (view_hourly.hourly_avg_market_value <= bounds.high)))
+GROUP BY view_hourly.item_id, (time_bucket('1 day'::interval, view_hourly.hourly_time_bucket));
